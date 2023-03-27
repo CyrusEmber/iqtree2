6cfab8c01671f638859558323e964e9baafdeeb7
Cyrus <Ke.Yan@anu.edu.au>
2023-03-28 07:57:49 +1100

    refactor

--- new/test_compare.py
+++ new/test_compare.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 
+cmd = []
 cmd1, log1, time1 = [], [], []
 cmd2, log2, time2 = [], [], []
 
@@ -10,6 +11,8 @@ with open("result1", "r") as result:
         log_likelihood, time = line.split()[-2], line.split()[-1]
         log1.append(log_likelihood)
         time1.append(time)
+        # parse the cmd
+        cmd.append(" ".join(line.split()[0: -2]))
         line = result.readline()
 result.close()
 
@@ -24,14 +27,14 @@ result.close()
 
 # plot
 plt.subplot(2, 1, 1)
-plt.plot(range(len(log1)), log1, label="iqtree1")
-plt.plot(range(len(log2)), log2, label="iqtree2")
+plt.plot(cmd, [float(log1[i]) - float(log2[i]) for i in range(len(log1))], label="difference")
+plt.axhline(0, color='red')
 plt.legend()
 plt.title('log likelihood')
-
 plt.subplot(2, 1, 2)
-plt.plot(range(len(time1)), time1, label="iqtree1")
-plt.plot(range(len(time2)), time2, label="iqtree2")
+
+plt.plot(cmd, [float(time1[i]) / float(time2[i]) for i in range(len(log1))], label="ratio")
+plt.axhline(1, color='red')
 plt.legend()
 plt.title('running time')
 plt.savefig('compare result.png')
